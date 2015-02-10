16e24c1e082deaf5cd7a3ce5effaf69e82b2c393
nologon <thijn@tripledotdiving.nl>
2015-02-10 14:18:49 +0100

    Update piglatin

--- piglatin.py
+++ piglatin.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
-x = "J123"
+pyg = 'ay'
 print "Welcome to the Pig Latin Translator!"
 # Start coding here!
 name = raw_input("What's your name? ")
 original = name
-print original
-if len(original) > 0 and x.isalpha():
-	print "nice word"
+if len(original) > 0 and original.isalpha():
+	print original
+	word = original.lower()
+	first = word[0]
+	new_word = word + first + pyg
+	s = new_word
+	new_word = s[1:len(new_word)]
+	print new_word
 else:
 	print "Empty"
