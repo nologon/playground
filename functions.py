51da84c1a3cb4e2ec7ad88f1b459ababdad9c05b
nologon <thijn@tripledotdiving.nl>
2015-02-11 11:23:56 +0100

    adjustedments

---
+++ functions.py
@@ -0,0 +1,15 @@
+#~ def one_good_turn(n):
+    #~ return n + 1
+    
+#~ def deserves_another(n):
+    #~ return one_good_turn(n) + 2
+    
+#~ one_good_turn(10)
+
+def fun_one(n):
+    return n * 5
+
+def fun_two(m):
+    return fun_one(m) + 7
+
+fun_one(1)
--- piglatin.py
+++ piglatin.py
@@ -2,10 +2,9 @@
 pyg = 'ay'
 print "Welcome to the Pig Latin Translator!"
 # Start coding here!
-name = raw_input("What's your name? ")
+name = raw_input("What word do you need translated? ")
 original = name
 if len(original) > 0 and original.isalpha():
-	print original
 	word = original.lower()
 	first = word[0]
 	new_word = word + first + pyg
