539391b5b843ae6d7390d60c7413a3881abe4bc6
nologon <thijn@tripledotdiving.nl>
2015-02-11 16:29:08 +0100

    New files

---
+++ shutdown.py
@@ -0,0 +1,9 @@
+def shutdown(s):
+    if s = "yes":
+        return "Shutting down"
+    elif s = "no":
+        return "Shutdown Aborted"
+    else:
+        return "Sorry"
+
+shutdown(yes)
---
+++ types.py
@@ -0,0 +1,7 @@
+# Print out the types of an integer, a float,
+# and a string on separate lines below.
+print type(17)
+print type("17")
+print type('whatever')
+print type("whatever")
+print type(0.223)
